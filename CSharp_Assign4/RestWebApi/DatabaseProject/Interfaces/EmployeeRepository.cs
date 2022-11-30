416a646becae32331787dc7b3f2ad31e83ea4f6d
MeghnaBaviskar <Meghna.Baviskar@emtecinc.com>
2022-11-30 18:12:15 +0530

    code added for Assignment of WebRestApi

---
+++ CSharp_Assign4/RestWebApi/DatabaseProject/Interfaces/EmployeeRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DatabaseProject.DatabaseContext;
+using DatabaseProject.Models;
+
+namespace DatabaseProject.Interfaces
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly SqlServerContext _sqlServerContext;
+
+        public EmployeeRepository(SqlServerContext sqlServerContext)
+        {
+            _sqlServerContext = sqlServerContext;
+        }
+
+        public List<Employee> GetEmployees()
+        {
+            var lstEmployees = _sqlServerContext.Employee.ToList();
+            return lstEmployees;
+        }
+
+        public Employee GetEmployeeById(int id)
+        {
+            var employee = _sqlServerContext.Employee.FirstOrDefault(x=>x.EId==id);
+            return employee;
+        }
+        
+        public Employee AddEmployee(Employee employee)
+        {
+            _sqlServerContext.Employee.Add(employee);
+            _sqlServerContext.SaveChanges();
+            return employee;
+        }
+    }
+}
---
+++ CSharp_Assign4/RestWebApi/DatabaseProject/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseProject.Models
+{
+    public class Employee
+    {
+        public int EId { get; set; }
+        public string ? EName { get; set; }
+        public string ? City { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
